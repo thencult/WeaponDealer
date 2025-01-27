96ac957b042a645a535a17c770c8cebf3278f3d1
Mapekins <mapkuss@inbox.lv>
2025-01-27 21:54:34 +0200

    Replaced placeholders with some possible values, Added materials

--- Assets/_Script/Item.cs
+++ Assets/_Script/Item.cs
@@ -6,35 +6,48 @@ public class Item : MonoBehaviour
 {
     private string Name;
     private string Description;
-    private List<ItemTypes.Type> Types;
+    private List<ItemTypes.Property> Properties;
+    private List<ItemTypes.Material> Materials;
     private ItemTypes.Rarity Rarity;
     private float AttackDamage;
     private float Price;
 
-    public void CalculateAttack(ItemTypes.Rarity rarity, List<ItemTypes.Type> types)
+    public void CalculateAttack(ItemTypes.Rarity rarity, List<ItemTypes.Property> properties, List<ItemTypes.Material> materials)
     {
         float sum = 0;
-        float modifiersSum = 0;
-        for (int i = 0; i < types.Count - 1; i++)
+        float PropertyModifiersSum = 1;
+        float MaterialModifiersSum = 1;
+        for (int i = 0; i < properties.Count - 1; i++)
         {
-            sum += ItemTypes.TypeBaseDamage.GetValueOrDefault(types[i]);
-            modifiersSum += ItemTypes.TypeDamageModifiers.GetValueOrDefault(types[i]);
+            sum += ItemTypes.PropertyBaseDamage.GetValueOrDefault(properties[i]);
+            PropertyModifiersSum *= ItemTypes.PropertyDamageModifiers.GetValueOrDefault(properties[i]);
+        }
+        for(int i = 0; i <  materials.Count - 1; i++)
+        {
+            sum += ItemTypes.MaterialBaseDamage.GetValueOrDefault(materials[i]);
+            MaterialModifiersSum *= ItemTypes.MaterialDamageModifiers.GetValueOrDefault(materials[i]);
         }
         sum += ItemTypes.AdditiveRarityDamage.GetValueOrDefault(rarity);
-        float result = sum * modifiersSum;
+        float result = sum * Mathf.Sqrt(MaterialModifiersSum * PropertyModifiersSum);
         AttackDamage = result;
     }
-    public void CalculatePrice(ItemTypes.Rarity rarity, List<ItemTypes.Type> types)
+    public void CalculatePrice(ItemTypes.Rarity rarity, List<ItemTypes.Property> properties, List<ItemTypes.Material> materials)
     {
         float sum = 0;
-        float modifiersSum = 0;
-        for (int i = 0; i < types.Count - 1; i++)
+        float PropertyModifiersSum = 1;
+        float MaterialModifiersSum = 1;
+        for (int i = 0; i < properties.Count - 1; i++)
         {
-            sum += ItemTypes.TypeBasePrice.GetValueOrDefault(types[i]);
-            modifiersSum += ItemTypes.TypePriceModifiers.GetValueOrDefault(types[i]);
+            sum += ItemTypes.PropertyBasePrice.GetValueOrDefault(properties[i]);
+            PropertyModifiersSum *= ItemTypes.PropertyPriceModifiers.GetValueOrDefault(properties[i]);
+        }
+        for(int i = 0; i < materials.Count - 1; i++)
+        {
+            sum += ItemTypes.MaterialBasePrice.GetValueOrDefault(materials[i]);
+            MaterialModifiersSum *= ItemTypes.MaterialPriceModifiers.GetValueOrDefault(materials[i]);
         }
         sum += ItemTypes.AdditiveRarityPrice.GetValueOrDefault(rarity);
-        float result = sum * modifiersSum;
+        float result = sum * Mathf.Sqrt(MaterialModifiersSum * PropertyModifiersSum);
         AttackDamage = result;
     }
 
@@ -42,8 +55,8 @@ public class Item : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        CalculateAttack(Rarity, Types);
-        CalculatePrice(Rarity, Types);
+        CalculateAttack(Rarity, Properties, Materials);
+        CalculatePrice(Rarity, Properties, Materials);
     }
 
     // Update is called once per frame
--- Assets/_Script/ItemTypes.cs
+++ Assets/_Script/ItemTypes.cs
@@ -5,60 +5,95 @@ using UnityEngine;
 public sealed class ItemTypes
 {
     // Added some library for the diffrent possible attributes in Items
-    public enum Type
+    public enum Property
     {
-        Slash,
-        Impact,
-        Puncture,
         Fire,
         Cold,
+        Poison,
         Electro,
-        Void
+        Gilded
     }
     // Base damage according to apect
-    public static Dictionary<ItemTypes.Type, float> TypeBaseDamage = new Dictionary<ItemTypes.Type, float>
+    public static Dictionary<ItemTypes.Property, float> PropertyBaseDamage = new Dictionary<ItemTypes.Property, float>
     {
-        {ItemTypes.Type.Slash, 3f},
-        {ItemTypes.Type.Impact, 3f},
-        {ItemTypes.Type.Puncture, 4f},
-        {ItemTypes.Type.Fire, 5f},
-        {ItemTypes.Type.Cold, 5f},
-        {ItemTypes.Type.Electro, 5f},
-        {ItemTypes.Type.Void, 7f}
+        {ItemTypes.Property.Fire, 12f},
+        {ItemTypes.Property.Cold, 8f},
+        {ItemTypes.Property.Electro, 8f},
+        {ItemTypes.Property.Poison, 5f},
+        {ItemTypes.Property.Gilded, 5f}
     };
     // Damage modifiers according to aspect type
-    public static Dictionary<ItemTypes.Type, float> TypeDamageModifiers = new Dictionary<ItemTypes.Type, float>
+    public static Dictionary<ItemTypes.Property, float> PropertyDamageModifiers = new Dictionary<ItemTypes.Property, float>
     {
-        {ItemTypes.Type.Slash, 1.4f},
-        {ItemTypes.Type.Impact, 1.4f},
-        {ItemTypes.Type.Puncture, 1.4f},
-        {ItemTypes.Type.Fire, 1.3f},
-        {ItemTypes.Type.Cold, 1.3f},
-        {ItemTypes.Type.Electro, 1.3f},
-        {ItemTypes.Type.Void, 1.15f}
+        {ItemTypes.Property.Fire, 1.1f},
+        {ItemTypes.Property.Cold, 1.2f},
+        {ItemTypes.Property.Electro, 1.2f},
+        {ItemTypes.Property.Poison, 1.5f},
+        {ItemTypes.Property.Gilded, 1f}
     };
     // Base money to be paid according to type
-    public static Dictionary<ItemTypes.Type, float> TypeBasePrice = new Dictionary<ItemTypes.Type, float>()
+    public static Dictionary<ItemTypes.Property, float> PropertyBasePrice = new Dictionary<ItemTypes.Property, float>()
     {
-        {ItemTypes.Type.Slash, 10f},
-        {ItemTypes.Type.Impact, 10f},
-        {ItemTypes.Type.Puncture, 10f},
-        {ItemTypes.Type.Fire, 15f},
-        {ItemTypes.Type.Cold, 15f},
-        {ItemTypes.Type.Electro, 15f},
-        {ItemTypes.Type.Void, 20f}
+        {ItemTypes.Property.Fire, 5f},
+        {ItemTypes.Property.Cold, 5f},
+        {ItemTypes.Property.Electro, 5f},
+        {ItemTypes.Property.Poison, 7f},
+        {ItemTypes.Property.Gilded, 10f}
     };
     // Money modifier according to type
-    public static Dictionary<ItemTypes.Type, float> TypePriceModifiers = new Dictionary<ItemTypes.Type, float>()
+    public static Dictionary<ItemTypes.Property, float> PropertyPriceModifiers = new Dictionary<ItemTypes.Property, float>()
     {
-        {ItemTypes.Type.Slash, 1.3f},
-        {ItemTypes.Type.Impact, 1.3f},
-        {ItemTypes.Type.Puncture, 1.3f},
-        {ItemTypes.Type.Fire, 1.2f},
-        {ItemTypes.Type.Cold, 1.2f},
-        {ItemTypes.Type.Electro, 1.2f},
-        {ItemTypes.Type.Void, 1.15f}
+        {ItemTypes.Property.Fire, 1.05f},
+        {ItemTypes.Property.Cold, 1.05f},
+        {ItemTypes.Property.Electro, 1.05f},
+        {ItemTypes.Property.Poison, 1.02f},
+        {ItemTypes.Property.Gilded, 1.2f}
     };
+    public enum Material
+    {
+        Wood,
+        Metal,
+        Bone,
+        Stone,
+        MCrystall
+    };
+
+    public static Dictionary<ItemTypes.Material, float> MaterialBaseDamage = new Dictionary<ItemTypes.Material, float>
+    {
+        {ItemTypes.Material.Wood, 3f},
+        {ItemTypes.Material.Metal, 5f},
+        {ItemTypes.Material.Bone, 4f},
+        {ItemTypes.Material.Stone, 4f},
+        {ItemTypes.Material.MCrystall, 3f}
+    };
+    // Damage modifiers according to aspect type
+    public static Dictionary<ItemTypes.Material, float> MaterialDamageModifiers = new Dictionary<ItemTypes.Material, float>
+    {
+        {ItemTypes.Material.Wood, 1.1f},
+        {ItemTypes.Material.Metal, 1.2f},
+        {ItemTypes.Material.Bone, 1.2f},
+        {ItemTypes.Material.Stone, 1.2f},
+        {ItemTypes.Material.MCrystall, 1.3f}
+    };
+    // Base money to be paid according to type
+    public static Dictionary<ItemTypes.Material, float> MaterialBasePrice = new Dictionary<ItemTypes.Material, float>()
+    {
+        {ItemTypes.Material.Wood, 1f},
+        {ItemTypes.Material.Metal, 3f},
+        {ItemTypes.Material.Bone, 2f},
+        {ItemTypes.Material.Stone, 2f},
+        {ItemTypes.Material.MCrystall, 3f}
+    };
+    // Money modifier according to type
+    public static Dictionary<ItemTypes.Material, float> MaterialPriceModifiers = new Dictionary<ItemTypes.Material, float>()
+    {
+        {ItemTypes.Material.Wood, 1.1f},
+        {ItemTypes.Material.Metal, 1.2f},
+        {ItemTypes.Material.Bone, 1.2f},
+        {ItemTypes.Material.Stone, 1.2f},
+        {ItemTypes.Material.MCrystall, 1.3f}
+    };
+
     // Possible rarities of the item
     public enum Rarity {
         Normal,
